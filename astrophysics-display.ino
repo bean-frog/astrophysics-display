f284569d5d56f8c65a42fe9c636baad25f6aa222
bean-frog <gkieran32855@gmail.com>
2024-12-04 13:33:16 -0800

    initial commit and license

---
+++ astrophysics-display.ino
@@ -0,0 +1,15 @@
+/*
+* Astrophysics Display - General Relativity
+* https://github.com/bean-frog/astrophysics-display
+* Copyright (c) 2024 by Graeme Kieran and Giacomo Resmini
+* MIT License - see LICENSE.md
+*/
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
